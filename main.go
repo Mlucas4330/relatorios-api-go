d31c0d91e888c038144a68c0e3fbd5869f8e5079
Lucas Medeiros <mlucas4330@gmail.com>
2023-02-07 17:50:03 -0300

    mudancas na estruturacao de pastas

--- main.go
+++ main.go
@@ -2,46 +2,31 @@ package main
 
 import (
 	"log"
-	"github.com/Mlucas4330/relatorios-api-go/controller"
+	"portifolio-api/database"
+	"portifolio-api/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func (r *Repository) SetupRoutes(app *fiber.App) {
-	api := app.Group("/api")
-	api.Post("/create_books", r.CreateBook)
-	api.Delete("delete_book/:id", r.DeleteBook)
-	api.Get("/get_books/:id", r.GetBookByID)
-	api.Get("/books", r.GetBooks)
+func setUpRoutes(app *fiber.App) {
+	app.Get("/", routes.Book)
+	app.Get("/allTickets", routes.AllBooks)
+	app.Post("/addTicket", routes.AddBook)
+	app.Put("/update", routes.Update)
+	app.Delete("/delete", routes.Delete)
 }
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err)
-	}
-	config := &storage.Config{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		Password: os.Getenv("DB_PASS"),
-		User:     os.Getenv("DB_USER"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
-		DBName:   os.Getenv("DB_NAME"),
-	}
-
-	db, err := storage.NewConnection(config)
-
-	if err != nil {
-		log.Fatal("could not load the database")
-	}
-	err = models.MigrateBooks(db)
-	if err != nil {
-		log.Fatal("could not migrate db")
-	}
-
-	r := Repository{
-		DB: db,
-	}
+	database.ConnectDb()
 	app := fiber.New()
-	r.SetupRoutes(app)
-	app.Listen(":8080")
+
+	setUpRoutes(app)
+
+	app.Use(cors.New())
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
